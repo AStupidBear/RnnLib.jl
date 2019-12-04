2f4da97fb271c05c5f2cb0b88ee9f654b5f20827
Yao Lu <luyaocns@gmail.com>
2019-12-05 01:08:12 +0800

    MLSuiteBase

--- src/RnnLib.jl
+++ src/RnnLib.jl
@@ -1,9 +1,9 @@
 module RnnLib
 
 using Distributed, Random, Statistics, Parameters
-using PyCall, HDF5, PyCallUtils, HDF5Utils
+using PyCall, HDF5, PyCallUtils, HDF5Utils, MLSuiteBase
 import ScikitLearnBase: BaseEstimator, fit!, predict, predict_proba, is_classifier
-import MLSuite: reset!, modelhash, signone
+import MLSuiteBase: reset!, modelhash
 
 export RnnModel, RnnClassifier, RnnRegressor
 

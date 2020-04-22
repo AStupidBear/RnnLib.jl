0342af77f2352d967e95ac84b83f692208a3efa5
Yao Lu <luyaocns@gmail.com>
2020-04-23 00:05:15 +0800

    numpy 1.17.0

--- deps/build.jl
+++ deps/build.jl
@@ -9,3 +9,4 @@ run(`$python -m pip install git+https://github.com/microsoft/onnxconverter-commo
 run(`$python -m pip install git+https://github.com/onnx/keras-onnx.git`)
 run(`$python -m pip install https://sourceforge.net/projects/bearapps/files/onnxruntime_dnnl-1.2.0-cp36-cp36m-linux_x86_64.whl`)
 run(`$python -m pip install numba==0.41 llvmlite==0.26 h5py hdf5plugin`)
+run(`$python -m pip install numpy==1.17.0`)

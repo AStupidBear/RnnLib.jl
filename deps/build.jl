34f8da552fbd9908e083c3736890453ca4733947
Yao Lu <luyaocns@gmail.com>
2020-06-26 01:47:09 +0800

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,6 @@
 using PyCall: python
 
-run(`$python -m pip install tensorflow keras onnx onnxruntime alt-model-checkpoint h5py hdf5plugin`)
+run(`$python -m pip install tensorflow==2.1.0 keras==2.3.1 keras-tcn keras-adamw alt-model-checkpoint h5py hdf5plugin`)
 run(`$python -m pip install --no-cache-dir horovod`)
-run(`$python -m pip install https://github.com/philipperemy/keras-tcn/archive/master.zip`)
-run(`$python -m pip install https://github.com/OverLordGoldDragon/keras-adamw/archive/master.zip`)
-run(`$python -m pip install https://github.com/microsoft/onnxconverter-common/archive/master.zip`)
-run(`$python -m pip install https://github.com/onnx/onnxmltools/archive/master.zip`)
-run(`$python -m pip install https://github.com/onnx/keras-onnx/archive/master.zip`)
+run(`$python -m pip install onnx onnxruntime onnxmltools keras2onnx`)
 run(`$python -m pip install numba==0.41 llvmlite==0.26`)

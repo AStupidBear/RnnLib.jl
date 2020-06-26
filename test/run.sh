eacf2bf9996072554a3f7016fdd211ba95e22a7c
Yao Lu <luyaocns@gmail.com>
2020-06-26 16:01:31 +0800

    update

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir=$(dirname $0)
 python $dir/../src/rnn.py --data_path=$HOME/job/train.rnn
-python $dir/ort.py 2> /dev/null
-python $dir/ort2.py 2> /dev/null
+python $dir/ort1d.py 2> /dev/null
+python $dir/ort2d.py 2> /dev/null
 python $dir/inception.py 2> /dev/null
 /bin/rm model.*

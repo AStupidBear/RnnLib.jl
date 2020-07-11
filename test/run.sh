2b92feceb4efb2d120fa0cfa73307e727fa00e31
Yao Lu <luyaocns@gmail.com>
2020-07-11 13:25:49 +0800

    update test

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dir=$(dirname $0)
-python $dir/../src/rnn.py 2> /dev/null
+python $dir/../src/rnn.py --epochs=1 2> /dev/null
 python $dir/ort1d.py 2> /dev/null
 python $dir/ort2d.py 2> /dev/null
 python $dir/inception.py 2> /dev/null

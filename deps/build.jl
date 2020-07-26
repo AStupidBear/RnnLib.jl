f1759afce589d74a91485e013d7951437d70b89e
Yao Lu <luyaocns@gmail.com>
2020-07-26 11:13:35 +0800

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -6,13 +6,13 @@ run(`$python -m pip install alt-model-checkpoint h5py hdf5plugin Cython`)
 run(`$python -m pip install "pandas<=0.25.3,!=0.24" "sktime<0.4"`)
 llvm_version = Base.libllvm_version
 if llvm_version >= v"9.0.0"
-    run(`$python -m pip install numba llvmlite==0.33`)
+    run(`$python -m pip install numba llvmlite`)
 elseif llvm_version >= v"7.0.0"
-    run(`$python -m pip install numba llvmlite==0.32`)
+    run(`$python -m pip install numba==0.49 llvmlite==0.32`)
 elseif llvm_version >= v"6.0.0"
-    run(`$python -m pip install numba llvmlite==0.26`)
+    run(`$python -m pip install numba==0.41 llvmlite==0.26`)
 elseif llvm_version >= v"5.0.0"
-    run(`$python -m pip install numba llvmlite==0.22`)
+    run(`$python -m pip install numba==0.38 llvmlite==0.22`)
 elseif llvm_version >= v"4.0.0"
-    run(`$python -m pip install numba llvmlite==0.20`)
+    run(`$python -m pip install numba==0.36.2 llvmlite==0.20`)
 end

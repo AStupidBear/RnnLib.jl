c5f535ab77a749c992ee1ae5bd6328287ea20823
Yao Lu <luyaocns@gmail.com>
2020-07-28 21:34:18 +0800

    update venv

--- deps/venv.sh
+++ deps/venv.sh
@@ -1,12 +1,2 @@
 virtualenv --copies ${JULIA_DEPOT_PATH-~/.julia}/../.virtualenv/horovod
 source ${JULIA_DEPOT_PATH-~/.julia}/../.virtualenv/horovod/bin/activate
-pip install https://github.com/AStupidBear/tensorflow-mkl/releases/download/2.1.0/tensorflow-compat-2.1.0-cp36-cp36m-linux_x86_64.whl
-pip install https://github.com/AStupidBear/onnxruntime-dnnl/releases/download/1.2/onnxruntime_dnnl-1.2.0-cp36-cp36m-linux_x86_64.whl
-pip install keras onnx alt-model-checkpoint h5py hdf5plugin torch
-pip install https://github.com/philipperemy/keras-tcn/archive/master.zip
-pip install https://github.com/OverLordGoldDragon/keras-adamw/archive/master.zip
-pip install https://github.com/microsoft/onnxconverter-common/archive/master.zip
-pip install https://github.com/onnx/onnxmltools/archive/master.zip
-pip install https://github.com/onnx/keras-onnx/archive/master.zip
-pip install --no-cache-dir horovod
-pip install numba==0.41 llvmlite==0.26 numpy==1.17.0
